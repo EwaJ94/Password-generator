af60cbd809cd56d3c692bb40f5a5dae28c5e69c6
EwaJochymkova <ewa.jochymkova@seznam.cz>
2023-10-12 13:08:03 +0200

    css + začátek js

---
+++ script.js
@@ -0,0 +1,11 @@
+let finalPassword = document.querySelector("#final-password")
+let length = document.querySelector("#length")
+let upperCase = document.querySelector("#upper-case")
+let lowerCase = document.querySelector("#lower-case")
+let numbers = document.querySelector("#numbers")
+let specialCharacters = document.querySelector("#special-characters")
+
+length.addEventListener("input", function(event){
+    let inputLength = event.target.value
+})
+
