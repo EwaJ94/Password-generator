b00ba8087c7e7d2c7f869d591150e292f995a068
EwaJochymkova <ewa.jochymkova@seznam.cz>
2023-10-18 17:34:48 +0200

    vypisování do inputu

--- js/script.js
+++ js/script.js
@@ -8,26 +8,42 @@ let generateButton = document.querySelector(".generator-btn")
 
 let newPassword = []
 
-// LENGTH RECORDING
-length.addEventListener("input", function(event){
+ // LENGTH RECORDING
+ length.addEventListener("input", function(event){
     let inputLength = event.target.value
 })
 
 // CHARACTERS RECORDING
 upperCase.addEventListener("change", function(event){
     let changeUpCase = event.target.checked
+    if (changeUpCase === true){
+        let randomUpCase = upperCaseArray[Math.floor(Math.random()*upperCaseArray.length)]
+        newPassword.push(randomUpCase)
+    }
 })
 
 lowerCase.addEventListener("change", function(event){
     let changeLoCase = event.target.checked
+    if (changeLoCase === true){
+        let randomLoCase = lowerCaseArray[Math.floor(Math.random()*lowerCaseArray.length)]
+        newPassword.push(randomLoCase)
+    }
 })
 
 numbers.addEventListener("change", function(event){
     let changeNumbers = event.target.checked
+    if (changeNumbers === true) {
+        let randomNumbers = numbersArray[Math.floor(Math.random()*numbersArray.length)]
+        newPassword.push(randomNumbers)
+    }
 })
 
 specialCharacters.addEventListener("change", function(event){
     let changeSpChar = event.target.checked
+    if (changeSpChar === true){
+        let randomSpChar = specialCharactersArray[Math.floor(Math.random()*specialCharactersArray.length)]
+        newPassword.push(randomSpChar)
+    }
 })
 
 
@@ -35,23 +51,17 @@ specialCharacters.addEventListener("change", function(event){
 // CREATING RANDOM PASSWORD
 generateButton.addEventListener("click", function(){
     
-    newPassword.length = 0
+    let myPassword = newPassword.join('')
 
-    let randomUpCase = upperCaseArray[Math.floor(Math.random()*upperCaseArray.length)]
+    let currentValue = finalPassword.value;
+    let newText = myPassword;
+    let updatedValue = currentValue + newText;
 
-    let randomLoCase = lowerCaseArray[Math.floor(Math.random()*lowerCaseArray.length)]
-
-    let randomNumbers = numbersArray[Math.floor(Math.random()*numbersArray.length)]
-
-    let randomSpChar = specialCharactersArray[Math.floor(Math.random()*specialCharactersArray.length)]
-
-    newPassword.push(randomUpCase, randomLoCase, randomNumbers, randomSpChar)
-
-    let myPassword = newPassword.toString()
-    finalPassword.textContent = myPassword
-
+    finalPassword.value = updatedValue;
     
-      
-      
+    
     console.log(myPassword)
+    
 })
+
+newPassword.length = 0
